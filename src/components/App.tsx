d98bf3618bb21a88f07a80bc3b7c427b0b3e5345
Alex Komanov <akomanov@seatgeek.com>
2021-07-07 19:51:16 +0300

    AK - Added class component AuthService

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { User } from "../model/Model";
+import { AuthService } from "../services/AuthService";
 
 interface AppState {
   user: User | undefined;
 }
 
 export class App extends React.Component<{}, AppState> {
+
+  private authService: AuthService = new AuthService();
   render() {
     return <div>App class works as well!</div>;
   }
